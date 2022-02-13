42b9dc160e33c5e6cff41e742951f559be3e755b
ehsankolivan1992 <eksapps@gmail.com>
2022-02-13 13:44:14 +0330

    project created

---
+++ app/src/main/java/com/ehsankolivand/todo_list780/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ehsankolivand.todo_list780
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
