e338d519f356d143c496dd4e2a2ead8528b3f4b5
ehsankolivan1992 <eksapps@gmail.com>
2022-02-14 11:03:26 +0330

    I've provided a room database in this module.

---
+++ app/src/main/java/com/ehsankolivand/todo_list780/core/di/modules/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.ehsankolivand.todo_list780.core.di.modules
+
+import android.content.Context
+import androidx.room.Room
+import com.ehsankolivand.todo_list780.model.database.TodoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ *In This Module, I've created a Room Database and provided it.
+ * */
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context)= Room.databaseBuilder(context,
+        TodoDatabase::class.java,
+        "todo_database")
+        .build()
+
+}
